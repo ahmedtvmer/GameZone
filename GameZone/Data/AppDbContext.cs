474eff9eb9963c5fcd21cb8fce61a8115de4602e
ahmedtvmer <ahmedhegazy4me@gmail.com>
2025-03-08 21:10:52 +0200

    Add project files.

---
+++ GameZone/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
